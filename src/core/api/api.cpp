6f667d264adf55b55e459479b8612a9cb745c756
DarekParodia <darekparodia@gmail.com>
2025-05-10 12:12:37 +0200

    Added OTA (not tested)

--- src/core/api/api.cpp
+++ src/core/api/api.cpp
@@ -11,6 +11,7 @@
 #error "Unsupported platform, please use ESP32 or ESP8266"
 #endif
 #include <WiFiManager.h>
+#include <ArduinoOTA.h>
 
 namespace core::api
 {
@@ -47,6 +48,66 @@ namespace core::api
     void setup()
     {
         enable();
+
+        if(!enabled)
+        {
+            Serial.println("Failed to enable API");
+            return;
+        }
+
+        // Setup OTA
+        ArduinoOTA.setPort(ota_port);
+
+        ArduinoOTA.onStart([]() {
+            String type;
+            if (ArduinoOTA.getCommand() == U_FLASH)
+            {
+                type = "sketch";
+            }
+            else
+            {
+                type = "filesystem";
+            }
+            Serial.println("Start updating " + type);
+        });
+
+        ArduinoOTA.onEnd([]() {
+            Serial.println("End update");
+            Serial.println("Rebooting...");
+            delay(1000);
+            ESP.restart();
+        });
+
+        ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+            Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+        });
+
+        ArduinoOTA.onError([](ota_error_t error) {
+            Serial.printf("Error[%u]: ", error);
+            if (error == OTA_AUTH_ERROR)
+            {
+                Serial.println("Auth Failed");
+            }
+            else if (error == OTA_BEGIN_ERROR)
+            {
+                Serial.println("Begin Failed");
+            }
+            else if (error == OTA_CONNECT_ERROR)
+            {
+                Serial.println("Connect Failed");
+            }
+            else if (error == OTA_RECEIVE_ERROR)
+            {
+                Serial.println("Receive Failed");
+            }
+            else if (error == OTA_END_ERROR)
+            {
+                Serial.println("End Failed");
+            }
+        });
+
+        ArduinoOTA.begin();
+        Serial.println("OTA setup complete");
     }
 
     void loop()
@@ -63,6 +124,8 @@ namespace core::api
         {
             server->loop();
         }
+
+        ArduinoOTA.handle();
     }
 
     void enable()
--- src/core/api/api.h
+++ src/core/api/api.h
@@ -7,6 +7,7 @@ namespace core::api
 {
     const std::string hostname = "GreenThing";
     const uint16_t webserver_port = 80;
+    const uint16_t ota_port = 8266;
 
     void setup();
     void loop();
