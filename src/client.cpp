701cc8c311801f844690054c79bebb8ee558111d
DarekParodia <darekparodia@gmail.com>
2025-07-03 12:45:57 +0200

    lcd basics

--- src/client.cpp
+++ src/client.cpp
@@ -10,11 +10,15 @@
 #include "mod/rcwl_1x05.h"
 #include "core/core.h"
 
-mod::Button button("button1", 5, true, true);         // Button on pin 2, inverted logic
+#include "I2C_LCD.h"
+
+I2C_LCD lcd(39);
+
+mod::Button button("button1", 5, true, true);            // Button on pin 2, inverted logic
 mod::Solenoid solenoid("solenoid1", 16, 14, false, 250); // Solenoid on pin 16 and 14, not inverted, pulse time 100ms
-mod::Led led("led1", 2, false, 1000);           // LED on pin 13, not inverted, toggle interval 1000ms
-mod::FlowMeter flow_meter("flow_meter1", 2);    // Flow meter on pin 2, default trigger point 512, default frequency per flow factor 5.5
-mod::Humidity humidity("humidity1", 4, true); // Humidity sensor on pin 4
+mod::Led led("led1", 2, false, 1000);                    // LED on pin 13, not inverted, toggle interval 1000ms
+mod::FlowMeter flow_meter("flow_meter1", 2);             // Flow meter on pin 2, default trigger point 512, default frequency per flow factor 5.5
+mod::Humidity humidity("humidity1", 4, true);            // Humidity sensor on pin 4
 // mod::Ultrasonic ultrasonic("ultrasonic1", 12, 13); // Ultrasonic sensor on pin 12 (trigger) and pin 13 (echo)
 mod::Ultrasonic *ultrasonic = new mod::RCWL_1x05("rcwl1x05", 21, 22); // RCWL-1X05 ultrasonic sensor on SDA pin 21 and SCL pin 22
 
@@ -28,6 +32,12 @@ namespace client
         core::addModule(&flow_meter);
         core::addModule(&humidity);
         core::addModule(ultrasonic);
+
+        lcd.begin(20, 4);
+
+        //  display fixed text once
+        lcd.setCursor(0, 0);
+        lcd.print("millis: ");
     }
 
     void loop()
@@ -47,5 +57,8 @@ namespace client
         }
 
         // Serial.println("Distance: " + String(ultrasonic->getDistance()) + " cm");
+
+        lcd.setCursor(8, 0);
+        lcd.print(millis());
     }
 }
