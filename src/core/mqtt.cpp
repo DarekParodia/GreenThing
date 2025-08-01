c2d89ddc2ff6b2c0b02ac4728243e57b1f3fc36a
DarekParodia <darekparodia@gmail.com>
2025-08-01 23:25:38 +0200

    mqtt base

--- src/core/mqtt.cpp
+++ src/core/mqtt.cpp
@@ -1,7 +1,38 @@
 #include "mqtt.h"
 
 namespace core::mqtt {
+    template <typename T>
+    mqtt_data<T>::mqtt_data(std::string topic) :
+        topic(topic) {}
+
+    template <typename T>
+    mqtt_data<T>::~mqtt_data() {}
+
+    template <typename T>
+    void mqtt_data<T>::update(T new_data) {
+        prev_data = data;
+        data      = new_data;
+        if(prev_data != data)
+            this->publish(std::to_string(data));
+    }
+
+    template <>
+    void mqtt_data<std::string>::update(std::string new_data) {
+        prev_data = data;
+        data      = new_data;
+        if(prev_data != data)
+            this->publish(data);
+    }
+
+    template <typename T>
+    void mqtt_data<T>::publish(std::string data) {
+    }
+
     void setup() {
+        core::wifi::addCustomParameter(&custom_mqtt_server);
+        core::wifi::addCustomParameter(&custom_mqtt_port);
+        core::wifi::addCustomParameter(&custom_mqtt_user);
+        core::wifi::addCustomParameter(&custom_mqtt_pass);
     }
     void loop() {
     }
--- src/core/mqtt.h
+++ src/core/mqtt.h
@@ -1,6 +1,32 @@
 #pragma once
 
+#include "core/wifi.h"
+
+#include <WiFiManager.h>
+#include <string>
+
 namespace core::mqtt {
+    // Custom parameters
+    WiFiManagerParameter custom_mqtt_server("server", "MQTT Server", mqtt_server, 40);
+    WiFiManagerParameter custom_mqtt_port("port", "MQTT Port", mqtt_port, 6);
+    WiFiManagerParameter custom_mqtt_user("user", "MQTT User", mqtt_user, 40);
+    WiFiManagerParameter custom_mqtt_pass("pass", "MQTT Password", mqtt_pass, 40);
+
+    template <typename T>
+    class mqtt_data {
+        public:
+            mqtt_data(std::string topic);
+            ~mqtt_data();
+            void update(T new_data);
+
+        private:
+            std::string topic;
+            T           data;
+            T           prev_data;
+            void        publish(std::string data);
+    };
+
     void setup();
     void loop();
+    void reconnect();
 } // namespace core::mqtt
